86d045f291d7a96419f5f81d32ea13db7dc10925
Nguyentuan3304 <22028209@vnu.edu.vn>
2024-12-18 01:12:33 +0700

    [UPDATE] update ticket creation timestamps in TicketField component

--- src/screens/TicketPage/TicketField/TicketField.jsx
+++ src/screens/TicketPage/TicketField/TicketField.jsx
@@ -33,8 +33,8 @@ const TicketField = () => {
                 <div className="ticket-list">
                     {tickets.map((ticket, index) => (
                         <>
-                            <TicketForm key={index} ticket={ticket} createAt={"2024-12-17 17:58:12.217419"} />
-                            <TicketForm key={index} ticket={ticket} createAt={"2024-12-17 17:56:12.217419"} />
+                            <TicketForm key={index} ticket={ticket} createAt={"2024-12-18 00:20:12.217419"} />
+                            <TicketForm key={index} ticket={ticket} createAt={"2024-12-18 00:28:12.217419"} />
                         </>
                     ))}
                 </div>
